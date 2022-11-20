3b821e5b347152c90b5167169c6f04d224b10be7
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2022-11-20 13:12:40 +0100

    new files

--- holberton.h
+++ holberton.h
@@ -1,52 +1,38 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-#include <stdlib.h>
 #include <stdarg.h>
-/**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
- */
-typedef struct flags
-{
-	int plus;
-	int space;
-	int hash;
-} flags_t;
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
 
 /**
- * struct printHandler - struct to choose the right function depending
- * on the format specifier passed to _printf()
- * @c: format specifier
- * @f: pointer to the correct printing function
- */
-typedef struct printHandler
+* struct types_arguments - Struct op
+* @t: The operator
+* @f: The function
+*/
+typedef struct types_arguments
 {
-	char c;
-	int (*f)(va_list ap, flags_t *f);
-} ph;
-int print_int(va_list l, flags_t *f);
-void print_number(int n);
-int print_unsigned(va_list l, flags_t *f);
-int count_digit(int i);
-int print_hex(va_list l, flags_t *f);
-int print_hex_big(va_list l, flags_t *f);
-int print_binary(va_list l, flags_t *f);
-int print_octal(va_list l, flags_t *f);
-char *convert(unsigned long int num, int base, int lowercase);
-int _printf(const char *format, ...);
-int (*get_print(char s))(va_list, flags_t *);
-int get_flag(char s, flags_t *f);
-int print_string(va_list l, flags_t *f);
-int print_char(va_list l, flags_t *f);
+	char t;
+	int (*f)(va_list vl);
+} funcs;
+
 int _putchar(char c);
-int _puts(char *str);
-int print_rot13(va_list l, flags_t *f);
-int print_rev(va_list l, flags_t *f);
-int print_bigS(va_list l, flags_t *f);
-int print_address(va_list l, flags_t *f);
-int print_percent(va_list l, flags_t *f);
+int print_char(va_list vl);
+int print_string(va_list vl);
+int print_number(va_list vl);
+void print_num_rec(int n);
+int print_rev(va_list vl);
+int print_rot13(va_list vl);
+int print_binary(va_list vl);
+int print_unsigned(va_list vl);
+int print_octal(va_list vl);
+int print_hex_lower(va_list vl);
+int print_hex_upper(va_list vl);
+int print_hex(unsigned long int c, int cap);
+int print_pointer(va_list vl);
+int print_special_chars(va_list vl);
+int _printf(const char *format, ...);
+
 #endif
